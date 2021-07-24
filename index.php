448218058a414b359cae2a1f2e10dbc8942e43bd
BuiltbyGit <eorgestomper22@gmail.com>
2021-07-24 23:14:28 +0100

    index commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/queries.css">
+	    <link rel="stylesheet" crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==">
+	<title>Muviedeck - Discover New Movies</title>
+</head>
+<body>
+	<nav class="navigationbar">
+                <ul class="menu">
+            <li class="logo"><a href="#">Muviedeck</a></li>
+            <li class="item"><a href="#">Home</a></li>
+            <li class="item"><a href="#" class="active">Popular</a></li>
+            <li class="item"><a href="#">Watchlist</a></li>
+            <li class="item login-cont"><button class="login-btn">Login</button></li>
+          <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+  </ul>
+      </nav>
+	<section class="movies-showcase" id="movies-showcase">
+		<div class="movie-showcase" style="
+		background-image: linear-gradient(rgb(7 14 39 / 20%) 100%,rgb(7 14 39 / 20%) 100%), url(https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg);
+		">
+			<div class="title-genre-year">
+				<h1>Black Widow</h1>
+				<div class="year-genre">
+					<p>2020</p>
+					<p>Adventure, Action, Sci-Fi</p>
+				</div>
+				<div class="movie-plot">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor diam nec justo pharetra feugiat. Proin ut condimentum dolor. Curabitur congue lectus ac arcu mollis, sed ultrices orci venenatis. Nunc pellentesque vehicula nisl egestas condimentum. Morbi elementum faucibus risus, quis congue magna posuere at.
+				</div>
+				<div class="selection-btn">
+					<button class="select-btn">Watch Now</button>
+					<button class="select-btn select-btn-transparent">Add to watchlist</button>
+				</div>
+			</div>
+		</div>
+		<div class="movie-showcase" style="
+		background-image: linear-gradient(rgb(7 14 39 / 20%) 100%,rgb(7 14 39 / 20%) 100%), url(https://m.media-amazon.com/images/M/MV5BNzQzYjIyZDQtMjBhZS00MzU3LTk0MTQtNTVmMDI3ZWY0ZWU3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg);
+		">
+			<div class="title-genre-year">
+				<h1>Movie Title</h1>
+				<div class="year-genre">
+					<p>2020</p>
+					<p>Adventure, Comedy</p>
+				</div>
+				<div class="movie-plot">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porttitor diam nec justo pharetra feugiat. Proin ut condimentum dolor. Curabitur congue lectus ac arcu mollis, sed ultrices orci venenatis. Nunc pellentesque vehicula nisl egestas condimentum. Morbi elementum faucibus risus, quis congue magna posuere at.
+				</div>
+				<div class="selection-btn">
+					<button class="select-btn">Watch Now</button>
+					<button class="select-btn select-btn-transparent">Add to watchlist</button>
+				</div>
+			</div>
+		</section>
+		<section class="popular-movies">
+			<h1 class="section-title">Popular Movies Right Now</h1>
+			<div class="movie-container">
+				<div class="movie-snip">
+					<div class="movie-img">
+					</div>
+					<h1>Black widow</h1>
+					<p><span class="meta"></span>Action</p>
+				</div>
+				<div class="movie-snip">
+					<div class="movie-img">
+					</div>
+					<h1>Black widow</h1>
+					<p><span class="meta"></span>Action</p>
+				</div>
+				<div class="movie-snip">
+					<div class="movie-img">
+					</div>
+					<h1>Black widow</h1>
+					<p><span class="meta"></span>Action</p>
+				</div>
+				<div class="movie-snip">
+					<div class="movie-img">
+					</div>
+					<h1>Black widow</h1>
+					<p><span class="meta"></span>Action</p>
+				</div>
+				<div class="movie-snip">
+					<div class="movie-img">
+					</div>
+					<h1>Black widow</h1>
+					<p><span class="meta"></span>Action</p>
+				</div>
+			</div>
+		</section>
+		<script src="assets/js/jquery-min.js"></script>
+		<script src="assets/js/main.js"></script>
+	</section>
+</body>
+</html>
